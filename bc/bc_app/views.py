434ff89021ba78e48d2eb64d17616e86c8b58bb7
Alexandr Gorbunov <asin.gorbunov@yandex.ru>
2023-10-25 21:01:25 +0300

    best practise

--- bc/bc_app/views.py
+++ bc/bc_app/views.py
@@ -4,15 +4,17 @@ from .models import Item
 
 # Create your views here.
 def index(request):
-    context = {}
-    return render(request, 'index.html', context=context)
+    context = {'asd': 'Privet'}
+    return render(request, 'bc_app/index.html', context=context)
 
 
 def pageItems(request):
     item = Item.objects.all()
-    return render(request, 'items.html', context={'item': item})
+    context = {'item': item}
+    return render(request, 'bc_app/items.html', context=context)
 
 
 def pageItem(request, id):
     item = get_object_or_404(Item, id=id)
-    return render(request, template_name='item.html', context={'item': item})
+    context = {'item': item}
+    return render(request, template_name='bc_app/item.html', context=context)
